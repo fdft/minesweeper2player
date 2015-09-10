aaa721f6ba167bf4b627b38295179f72a538f7f9
Brandon <noemail@gmail.com>
2015-09-10 14:47:16 -0700

    had two player support. now need to figure out how to connect this with firebase.

--- ms.js
+++ ms.js
@@ -4,14 +4,15 @@ myApp.controller('GameController', ['$scope', '$firebaseObject',
 function($scope, $firebaseObject) {
 	$scope.PLAYER1 = "1";
 	$scope.PLAYER2 = "2";
+	$scope.currentPlayer = $scope.PLAYER1;
     $scope.alive = "";
     $scope.side = 8;
-    // states:
-    //  0 - hidden empty
-    //  1 - hidden mine
-    //  2 - revealed empty
-    //  3 - revealed mine
-    //  4 - missed empty
+    $scope.tileType = { 'hiddenEmpty': 0,
+    					'hiddenMine': 1,
+    					'revealedEmpty': 2,
+    					'revealedMine1': 3,
+    					'revealedMine2': 4,
+    					'missedEmpty': 5};
     $scope.state = new Array();
 
     $scope.range = function () {
@@ -21,17 +22,30 @@ function($scope, $firebaseObject) {
     };
 
     $scope.reveal = function (row, col) {
-        if ($scope.state[row][col].value == 1) {
-            $scope.pwnd();
-        } else if ($scope.state[row][col].value == 0) {
+        if ($scope.state[row][col].value == $scope.tileType['hiddenMine']) {
+            //$scope.pwnd();
+            if ( $scope.currentPlayer == $scope.PLAYER1) {
+            	$scope.state[row][col].value = $scope.tileType['revealedMine1'];
+            } else {
+            	$scope.state[row][col].value = $scope.tileType['revealedMine2'];
+            }
+        } else if ($scope.state[row][col].value == $scope.tileType['hiddenEmpty']) {
             $scope.tryreveal(row, col);
+        } else {
+        	return; // Don't do anything.
+        }
+        
+        if ($scope.currentPlayer == $scope.PLAYER1 ) {
+        	$scope.currentPlayer = $scope.PLAYER2;
+        } else {
+        	$scope.currentPlayer = $scope.PLAYER1;
         }
     };
 
     $scope.tryreveal = function (row, col) {
         if (row < 0 || row >= $scope.side || col < 0 || col >= $scope.side) return;
 
-        if ($scope.state[row][col].value == 0) {
+        if ($scope.state[row][col].value == $scope.tileType['hiddenEmpty']) {
             $scope.state[row][col].value = 2;
 
             if ($scope.isz(row - 1, col - 1) && 
@@ -57,9 +71,9 @@ function($scope, $firebaseObject) {
     $scope.pwnd = function () {
         for(var i = 0; i< $scope.side; i++) {
             for(var j = 0; j < $scope.side; j++) {
-                if($scope.state[i][j].value == 0) {
+                if($scope.state[i][j].value == $scope.tileType['hiddenEmpty']) {
                     $scope.state[i][j].value = 2;
-                } else if($scope.state[i][j].value == 1) {
+                } else if($scope.state[i][j].value == $scope.tileType['hiddenMine']) {
                     $scope.state[i][j].value = 3;
                 }
             }
@@ -96,15 +110,18 @@ function($scope, $firebaseObject) {
         if (row < 0 || row >= $scope.side || col < 0 || col >= $scope.side)
             return true;
 
-        return $scope.state[row][col].value == 0 || 
-               $scope.state[row][col].value == 2 ||
-               $scope.state[row][col].value == 4;
+        return $scope.state[row][col].value == $scope.tileType['hiddenEmpty'] || 
+               $scope.state[row][col].value == $scope.tileType['revealedEmpty']  ||
+               $scope.state[row][col].value == $scope.tileType['missedEmpty'];
     };
 
-    $scope.ct = function (st, row, col) {
-        if ($scope.state[row][col].value == 0 || $scope.state[row][col].value == 1 || $scope.state[row][col].value == 3)
+    $scope.printTile = function (st, row, col) {
+        if ($scope.state[row][col].value == $scope.tileType['hiddenEmpty'] 
+        	|| $scope.state[row][col].value == $scope.tileType['hiddenMine']
+        	|| $scope.state[row][col].value == $scope.tileType['revealedMine1']
+        	|| $scope.state[row][col].value == $scope.tileType['revealedMine2'])
             return "";
-        
+
         var ret = 
             !$scope.isz(row - 1, col - 1) + 
             !$scope.isz(row - 1, col) + 
@@ -122,14 +139,22 @@ function($scope, $firebaseObject) {
         return ret;
     };
 
-    $scope.cf = function (st) {
-        if (st.value == 0 || st.value == 1)
+    $scope.showBomb = function(st) {
+    	if (st.value == $scope.tileType['revealedMine1']
+    		|| st.value == $scope.tileType['revealedMine2'])
+    		return true;
+    	return false;
+    }
+    $scope.getCSSClass = function (st) {
+        if (st.value == $scope.tileType['hiddenMine'] || st.value == $scope.tileType['hiddenEmpty'])
             return 'gd gd-hidden';
-        else if (st.value == 2)
+        else if (st.value == $scope.tileType['revealedEmpty'])
             return 'gd gd-show';
-        else if (st.value == 3)
-            return 'gd gd-show-mine icon-certificate';
-        else if (st.value == 4)
+        else if (st.value == $scope.tileType['revealedMine1'])
+            return 'gd gd-show-mine1';
+        else if (st.value == $scope.tileType['revealedMine2'])
+         	return 'gd gd-show-mine2';
+        else if (st.value == $scope.tileType['missedEmpty'])
             return 'gd gd-show-missed';
     };
 
@@ -138,7 +163,7 @@ function($scope, $firebaseObject) {
             $scope.state[i] = new Array($scope.side);
             for (var j = 0; j < $scope.side; j++) {
                 $scope.state[i][j] = {
-                    value: 0
+                    value: $scope.tileType['hiddenEmpty']
                 };
             }
         }
@@ -150,7 +175,7 @@ function($scope, $firebaseObject) {
         $scope.alive = "";
         for (var i = 0; i < $scope.side; i++) {
             for (var j = 0; j < $scope.side; j++) {
-                $scope.state[i][j].value = 0;
+                $scope.state[i][j].value = $scope.tileType['hiddenEmpty'];
             }
         }
 
@@ -163,9 +188,18 @@ function($scope, $firebaseObject) {
             var currentrow = Math.floor(Math.random() * ($scope.side));
             var currentcol = Math.floor(Math.random() * ($scope.side));
 
-            if ($scope.state[currentrow][currentcol].value != 1) {
-                $scope.state[currentrow][currentcol].value = 1;
+            if ($scope.state[currentrow][currentcol].value != $scope.tileType['hiddenMine']) {
+                $scope.state[currentrow][currentcol].value = $scope.tileType['hiddenMine'];
                 randscreated++;
+            }
+        }
+    };
+    
+    $scope.debugClickAllNonBombs = function () {
+        for (var i = 0; i < $scope.side; i++) {
+            for (var j = 0; j < $scope.side; j++) {
+                if ( $scope.state[i][j].value == $scope.tileType['hiddenEmpty'])
+                	$scope.state[i][j].value = $scope.tileType['revealedEmpty']
             }
         }
     };
